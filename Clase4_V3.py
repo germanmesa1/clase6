9a556ccd587b308bf5a320cd57949ead0e17b832
Biblioteca <biblioteca@docencia.local>
2025-04-01 18:42:55 -0500

    Implementación de mejoras en el sistema veterinario: - Prevención de medicamentos duplicados - Gestión de mascotas caninas y felinas mediante diccionarios - Validación de fecha en formato dd/mm/aaaa - Función para eliminar medicamento de una mascota

--- Clase4_V3.py
+++ Clase4_V3.py
@@ -1,11 +1,14 @@
+import datetime
+
 class Paciente:
     def __init__(self):
         self.__nombre = '' 
         self.__cedula = 0 
         self.__genero = '' 
         self.__servicio = '' 
-              
-    #metodos get    
+        self.__medicamentos = []  # Lista para almacenar medicamentos
+        
+    # Métodos get    
     def verNombre(self):
         return self.__nombre 
     def verCedula(self):
@@ -14,92 +17,121 @@ class Paciente:
         return self.__genero 
     def verServicio(self):
         return self.__servicio 
-    # metodos set
-    def asignarNombre(self,n):
+    def verMedicamentos(self):
+        return self.__medicamentos 
+    
+    # Métodos set
+    def asignarNombre(self, n):
         self.__nombre = n 
-    def asignarCedula(self,c):
+    def asignarCedula(self, c):
         self.__cedula = c 
-    def asignarGenero(self,g):
+    def asignarGenero(self, g):
         self.__genero = g 
-    def asignarServicio(self,s):
+    def asignarServicio(self, s):
         self.__servicio = s 
+    
+    def agregarMedicamento(self, medicamento):
+        if medicamento in self.__medicamentos:
+            print("Este medicamento ya está registrado.")
+        else:
+            self.__medicamentos.append(medicamento)
+    
+    def eliminarMedicamento(self, medicamento):
+        if medicamento in self.__medicamentos:
+            self.__medicamentos.remove(medicamento)
+            print("Medicamento eliminado correctamente.")
+        else:
+            print("El medicamento no está en la lista.")
         
 class Sistema:    
     def __init__(self):
         self.__lista_pacientes = [] 
+        self.__caninos = {}
+        self.__felinos = {}
         
-    def verificarPaciente(self,cedula):
+    def verificarPaciente(self, cedula):
         for p in self.__lista_pacientes:
             if cedula == p.verCedula():
                 return True 
         return False
-        
-    def ingresarPaciente(self,pac):
+    
+    def ingresarPaciente(self, pac):
         self.__lista_pacientes.append(pac)
         return True
     
     def verDatosPaciente(self, c):
-        if self.verificarPaciente(c) == False:
+        if not self.verificarPaciente(c):
             return None
         for p in self.__lista_pacientes:
-            #retorne la cedula y la comparo con la ingresada por teclado
             if c == p.verCedula():
-                return p #si encuentro el paciente lo retorno
-            
+                return p
+    
     def verNumeroPacientes(self):
         print("En el sistema hay: " + str(len(self.__lista_pacientes)) + " pacientes") 
+    
+    def validarFecha(self, fecha):
+        try:
+            datetime.datetime.strptime(fecha, "%d/%m/%Y")
+            return True
+        except ValueError:
+            return False
 
 def main():
     sis = Sistema() 
-    #probemos lo que llevamos programado
     while True:
-        #TAREA HACER EL MENU
-        opcion = int(input("\nIngrese \n0 para salir, \n1 para ingresar nuevo paciente, \n2 ver Paciente\n\t--> ")) 
+        opcion = int(input("\nIngrese \n0 para salir, \n1 para ingresar nuevo paciente, \n2 ver Paciente, \n3 agregar medicamento, \n4 eliminar medicamento\n\t--> ")) 
         
         if opcion == 1:
-            #ingreso pacientes
-            print("A continuacion se solicitaran los datos ...") 
-            #1. Se solicitan los datos
-            cedula = int(input("Ingrese la cedula: ")) 
+            print("A continuación se solicitarán los datos ...") 
+            cedula = int(input("Ingrese la cédula: ")) 
             if sis.verificarPaciente(cedula):
-                print("\n<< Ya existe un paciente con esa cedula >>".upper()) 
+                print("\n<< Ya existe un paciente con esa cédula >>".upper()) 
             else:    
-                # 2. se crea un objeto Paciente
                 pac = Paciente() 
-                # como el paciente esta vacio debo ingresarle la informacion
                 pac.asignarNombre(input("Ingrese el nombre: ")) 
                 pac.asignarCedula(cedula) 
-                pac.asignarGenero(input("Ingrese el genero: ")) 
+                pac.asignarGenero(input("Ingrese el género: ")) 
                 pac.asignarServicio(input("Ingrese servicio: ")) 
-                #3. se almacena en la lista que esta dentro de la clase sistema
                 r = sis.ingresarPaciente(pac)             
                 if r:
                     print("Paciente ingresado") 
                 else:
                     print("No ingresado") 
         elif opcion == 2:
-            #1. solicito la cedula que quiero buscar
-            c = int(input("Ingrese la cedula a buscar: ")) 
-            #le pido al sistema que me devuelva en la variable p al paciente que tenga
-            #la cedula c en la lista
+            c = int(input("Ingrese la cédula a buscar: ")) 
             p = sis.verDatosPaciente(c) 
-            #2. si encuentro al paciente imprimo los datos
-            if p != None:
+            if p:
                 print("Nombre: " + p.verNombre()) 
-                print("Cedula: " + str(p.verCedula())) 
-                print("Genero: " + p.verGenero()) 
+                print("Cédula: " + str(p.verCedula())) 
+                print("Género: " + p.verGenero()) 
                 print("Servicio: " + p.verServicio()) 
+                print("Medicamentos: " + ", ".join(p.verMedicamentos()) if p.verMedicamentos() else "Sin medicamentos")
             else:
-                print("No existe un paciente con esa cedula") 
-        elif opcion !=0:
+                print("No existe un paciente con esa cédula") 
+        elif opcion == 3:
+            c = int(input("Ingrese la cédula del paciente: "))
+            p = sis.verDatosPaciente(c)
+            if p:
+                medicamento = input("Ingrese el nombre del medicamento: ")
+                p.agregarMedicamento(medicamento)
+            else:
+                print("Paciente no encontrado.")
+        elif opcion == 4:
+            c = int(input("Ingrese la cédula del paciente: "))
+            p = sis.verDatosPaciente(c)
+            if p:
+                medicamento = input("Ingrese el nombre del medicamento a eliminar: ")
+                p.eliminarMedicamento(medicamento)
+            else:
+                print("Paciente no encontrado.")
+        elif opcion != 0:
             continue 
         else:
             break 
 
-#aca el python descubre cual es la funcion principal
 if __name__ == "__main__":
-    main() 
-        
+    main()
+
         
         
         
